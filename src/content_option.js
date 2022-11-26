f94287bb43529ed7367c4c6d8608542440ee57a4
ZERstar <dumbretejas1@gmail.com>
2022-11-26 22:25:48 +0530

    .

---
+++ src/content_option.js
@@ -0,0 +1,125 @@
+const logotext = "R";
+const meta = {
+    title: "Roshan Shaji",
+    description: "I’m Roshan Shaji Front-End devloper,currently studying in Chennai",
+};
+
+const introdata = {
+    title: "I’m Roshan Shaji",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am highly organized. I always take notes, and I use a series of tools to help myself stay on top of deadlines. I am ambitious and driven. I thrive on challenges and constantly set goals for myself, so I have something to strive toward.",
+
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am highly organized. I always take notes, and I use a series of tools to help myself stay on top of deadlines. I am ambitious and driven. I thrive on challenges and constantly set goals for myself, so I have something to strive toward.",
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YX",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "V",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "X",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "Python",
+    value: 90,
+},
+{
+    name: "Djano",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "Jquery",
+    value: 85,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Mobile Apps",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/id/119/500/500",
+    desctiption: "IBM Python for Data Science and AI.",
+    link: "https://www.coursera.org/account/accomplishments/verify/9577NT88V39A",
+},
+
+{
+    img: "https://picsum.photos/id/0/500/500",
+    desctiption: "IP-Locator (Computer Networks Project).",
+    link: "https://github.com/roshann00/IP-Locator",
+},
+
+{
+    img: "https://picsum.photos/id/20/500/500",
+    desctiption: "Amazon Certification.",
+    link: "https://www.credly.com/badges/f9da6f6e-7e9d-4ed3-94c6-0ed8b10ec192/print",
+},
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "roshanshaji2002@gmail.com",
+    YOUR_FONE: "xxxxxxxxxx",
+    description: " ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
